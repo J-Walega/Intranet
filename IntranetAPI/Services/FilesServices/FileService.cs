6039e79646200d367220944f4ed57d2d2a18c6c7
JW-39615 <56237477+JW-39615@users.noreply.github.com>
2021-06-14 10:49:23 +0200

    Added check if uploaded file exists

--- IntranetAPI/Services/FilesServices/FileService.cs
+++ IntranetAPI/Services/FilesServices/FileService.cs
@@ -11,7 +11,7 @@ namespace IntranetAPI.Services.FilesServices
 {
     public class FileService : IFileService
     {
-        private IFileRepo _repo;
+        private readonly IFileRepo _repo;
         private readonly IWebHostEnvironment _enviroment;
 
         public FileService(IFileRepo repo, IWebHostEnvironment enviroment)
@@ -30,13 +30,19 @@ namespace IntranetAPI.Services.FilesServices
                     Path = $"/Uploads/{request.Category}/{request.File.FileName}"
                 };
                 System.IO.Directory.CreateDirectory($"Uploads//{request.Category}");
-                using (System.IO.FileStream fileStream = System.IO.File.Create(_enviroment.ContentRootPath + "\\Uploads\\" + request.Category + "\\" + request.File.FileName))
+                string path = _enviroment.ContentRootPath + "\\Uploads\\" + request.Category + "\\" + request.File.FileName;
+                var info = new System.IO.FileInfo(path);
+
+                using System.IO.FileStream fileStream = System.IO.File.Create(path);
+                if (!info.Exists)
                 {
                     await request.File.CopyToAsync(fileStream);
                     fileStream.Flush();
+                    // Uncomment to save in DB
+                    // await _repo.SaveFileAsync(file);
+                    return true;
                 }
-                // await _repo.SaveFileAsync(file);
-                return true;
+                return false;
             }
             catch(Exception ex)
             {
