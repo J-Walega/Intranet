bdd9370c40cc9c1e51cbfa0f750110d5b083b9d6
JW-39615 <56237477+JW-39615@users.noreply.github.com>
2021-06-14 12:14:58 +0200

    migration added

---
+++ IntranetAPI/Migrations/20210614101330_files_migration.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IntranetAPI.Migrations
+{
+    public partial class files_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "Links",
+                newName: "Url");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Links",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "Links",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Category = table.Column<int>(type: "int", nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UploadDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Links");
+
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Links",
+                newName: "Path");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Links",
+                newName: "Name");
+        }
+    }
+}
