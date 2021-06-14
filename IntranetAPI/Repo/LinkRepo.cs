fe48647885951d2673d4281dc55c0731c2110619
JW-39615 <56237477+JW-39615@users.noreply.github.com>
2021-06-14 10:59:53 +0200

    initial

---
+++ IntranetAPI/Repo/LinkRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntranetAPI.Repo
+{
+    public class LinkRepo
+    {
+        private readonly DataContext _context;
+        public LinkRepo(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ IntranetAPI/Services/LinksServices/LinkService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntranetAPI.Services.LinksServices
+{
+    public class LinkService
+    {
+    }
+}
