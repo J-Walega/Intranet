de637aa03c57ccd2598660fd049234b86179cdba
JW-39615 <56237477+JW-39615@users.noreply.github.com>
2021-06-16 09:31:37 +0200

    Initail

---
+++ IntranetAPI/Contracts/V1/Requests/Phone/AddPhoneRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntranetAPI.Contracts.V1.Requests.Phone
+{
+    public class AddPhoneRequest
+    {
+        [Required]
+        public string Name{ get; set; }
+        [Required]
+        public int Number { get; set; }
+    }
+}
