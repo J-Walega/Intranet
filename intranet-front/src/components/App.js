8630d112f890df4fc261790fb0294000bd914f15
JW-39615 <56237477+JW-39615@users.noreply.github.com>
2021-06-16 09:00:51 +0200

    routing

--- intranet-front/src/components/App.js
+++ intranet-front/src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
 import Navigation from './navbar/Navigation';
 
 class App extends React.Component{
   render() {
     return(
-      <Navigation />
+      <Router>
+        <div>
+          <Navigation/>
+          <Switch>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
