65368459fa6d86ebe0fcb01a76f8f9d4e7ad05cf
JW-39615 <56237477+JW-39615@users.noreply.github.com>
2021-06-29 10:13:49 +0200

    messin'

--- intranet-front/src/components/App.js
+++ intranet-front/src/components/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './login/Login';
 import Phonebook from './phonebook/Phonebook';
 import Navigation from './navbar/Navigation';
 
-class App extends React.Component{
-  render() {
-    return(
-      <Router>
-          <Navigation/>
-          <Switch>
-            <Route path="/phonebook" component={Phonebook}/>
-            <Route path="/login" component={Login}/>
-          </Switch>
-      </Router>
-    );
-  }
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken))
 }
 
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
+function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  
+  return(
+    <Router>
+        <Navigation/>
+        <Switch>
+          <Route path="/phonebook" component={Phonebook}/>
+          <Route path="/login" component={Login}/>
+        </Switch>
+    </Router>
+  );
+}
+  
 export default App;
--- intranet-front/src/components/login/Login.js
+++ intranet-front/src/components/login/Login.js
@@ -1,27 +1,52 @@
-import React from 'react'
-import {Form, FormGroup, Button, Container} from 'react-bootstrap'
-import './Login.css'
+import axios from 'axios';
+import React, {useState} from 'react';
+import {Form, FormGroup, Button, Container} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import './Login.css';
 
-export default class Login extends React.Component {
+async function loginUser(credentials) {
+    return axios.post(
+        'https://localhost:44332/api/v1/login', {
+            credentials
+        }
+    )
+    .then((response) => {
+        console.log(response);
+    },  (error) => {
+        console.log(error);
+    });
+}
 
-    render() {
-        return(
-            <Container>
-                <Form className = "login-form">
-                    <FormGroup controlId = "formBasicUsername">
-                        <Form.Label>Login</Form.Label>
-                        <Form.Control type = "text" placeholder = "Wpisz login" />
-                    </FormGroup>
-                    <FormGroup controlId = "formBasicPassword">
-                        <Form.Label>Hasło</Form.Label>
-                        <Form.Control type = "password" placeholder = "Wpisz hasło" />
-                    </FormGroup>
-                    <Button variant = "primary" type = "submit"
-                    className = "login-button">
-                        Zaloguj
-                    </Button>
-                </Form>
-            </Container>       
-        )
+
+export default function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
     }
+
+    return(
+        <Container>
+            <Form className = "login-form" onSubmit={handleSubmit}>
+                <FormGroup controlId = "formBasicUsername">
+                    <Form.Label>Login</Form.Label>
+                    <Form.Control type = "text" placeholder = "Wpisz login" onChange={e => setUserName(e.target.value)}/>
+                </FormGroup>
+                <FormGroup controlId = "formBasicPassword">
+                    <Form.Label>Hasło</Form.Label>
+                    <Form.Control type = "password" placeholder = "Wpisz hasło" onChange={e => setPassword(e.target.value)}/>
+                </FormGroup>
+                <Button variant = "primary" type = "submit"
+                className = "login-button">
+                    Zaloguj
+                </Button>
+            </Form>
+        </Container>       
+    )
 }
