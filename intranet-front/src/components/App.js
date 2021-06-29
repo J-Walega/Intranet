ac34a3a996b5ee2f22fd3228175b36047780a867
JW-39615 <56237477+JW-39615@users.noreply.github.com>
2021-06-29 12:03:53 +0200

    token storage changed to local storage

--- intranet-front/src/components/App.js
+++ intranet-front/src/components/App.js
@@ -6,11 +6,11 @@ import Phonebook from './phonebook/Phonebook';
 import Navigation from './navbar/Navigation';
 
 function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken))
+  localStorage.setItem('token', JSON.stringify(userToken))
 }
 
 function getToken() {
-  const tokenString = sessionStorage.getItem('token');
+  const tokenString = localStorage.getItem('token');
   const userToken = JSON.parse(tokenString);
   return userToken?.token
 }
